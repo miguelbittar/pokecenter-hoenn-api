5d19e1f5b3f695a23bf15f591494ab3da874d0b2
Miguel Bittar <miguelbittar29@gmail.com>
2025-08-22 16:08:52 -0300

    feat: initial config

---
+++ src/main/java/dev/miguel/pokecenter_hoenn_api/PokecenterHoennApiApplication.java
@@ -0,0 +1,13 @@
+package dev.miguel.pokecenter_hoenn_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokecenterHoennApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokecenterHoennApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/miguel/pokecenter_hoenn_api/PokecenterHoennApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.miguel.pokecenter_hoenn_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokecenterHoennApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
