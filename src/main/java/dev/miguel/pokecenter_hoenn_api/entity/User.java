abf0179ddfe0b790a5a5d5cee503c84ea33922d3
Miguel Bittar <miguelbittar29@gmail.com>
2025-08-22 17:21:45 -0300

    feat: add User entity and UserRole enum

---
+++ src/main/java/dev/miguel/pokecenter_hoenn_api/entity/User.java
@@ -0,0 +1,39 @@
+package dev.miguel.pokecenter_hoenn_api.entity;
+
+
+import dev.miguel.pokecenter_hoenn_api.util.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private UserRole role;
+
+}
